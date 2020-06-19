529c32fde60b88cee8829cfe4a4356671deab6e0
patheomwiko <patheomwiko45@gmail.com>
2020-06-19 15:23:37 +0200

    modification admin

--- application/views/admin/profile.php
+++ application/views/admin/profile.php
@@ -36,7 +36,18 @@
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top m-b-0">
             <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></a>
-            <div class="top-left-part"><a class="logo" href="index.html"><b><img src="../plugins/images/pixeladmin-logo.png" alt="Dim" /></b><span class="hidden-xs"><img src="../plugins/images/pixeladmin-text.png" alt=" CrowdFunding" /></span></a></div>                <ul class="nav navbar-top-links navbar-left m-l-20 hidden-xs">
+            <div class="top-left-part">
+                    <a class="logo" href="index.html">
+                        <b> 
+                            DIM
+                            <!-- <img src="<?=base_url('assets/admin/plugins/images/pixeladmin-logo.png');?>" alt="home" /> -->
+                        </b>
+                        <span class="hidden-xs">
+                            social
+                            <!-- <img src="<?=base_url('assets/admin/plugins/images/pixeladmin-text.png');?>" alt="home" /> -->
+                        </span>
+                    </a>
+                </div>
                     <li>
                         <!-- <form role="search" class="app-search hidden-xs">
                             <input type="text" placeholder="Search..." class="form-control"> <a href=""><i class="fa fa-search"></i></a>
