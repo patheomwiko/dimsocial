7ae381b7bff1d965f78fdcda18573acf3429acc2
patheomwiko <patheomwiko45@gmail.com>
2020-06-19 14:52:10 +0200

    modification admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -40,7 +40,7 @@
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top m-b-0">
             <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></a>
-                <div class="top-left-part"><a class="logo" href="index.html"><b><img src="<?=base_url('assets/admin/images/');?>" alt="Dim" /></b><span class="hidden-xs"><img src="<?=base_url('assets/admin/images/');?>" alt=" CrowdFunding" /></span></a></div>
+                <div class="top-left-part"><a class="logo" href="index.html"><b><img   alt="Dim" /></b><span class="hidden-xs"><img src="<?=base_url('assets/admin/images/');?>"  alt=" CrowdFunding" /></span></a></div>
                 <ul class="nav navbar-top-links navbar-left m-l-20 hidden-xs">
                     <li>
                         <!-- <form role="search" class="app-search hidden-xs">
