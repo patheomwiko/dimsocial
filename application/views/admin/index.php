4046a3ce94bd5217ba944c3d4476645671e02716
patheomwiko <patheomwiko45@gmail.com>
2020-06-19 15:18:42 +0200

    modification admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -40,7 +40,18 @@
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top m-b-0">
             <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></a>
-                <div class="top-left-part"><a class="logo" href="index.html"><b><img   alt="Dim" /></b><span class="hidden-xs"><img src="<?=base_url('assets/admin/images/');?>"  alt=" CrowdFunding" /></span></a></div>
+            <div class="top-left-part">
+                    <a class="logo" href="index.html">
+                        <b> 
+                            DIM
+                            <!-- <img src="<?=base_url('assets/admin/plugins/images/pixeladmin-logo.png');?>" alt="home" /> -->
+                        </b>
+                        <span class="hidden-xs">
+                            Social
+                            <!-- <img src="<?=base_url('assets/admin/plugins/images/pixeladmin-text.png');?>" alt="home" /> -->
+                        </span>
+                    </a>
+                </div>
                 <ul class="nav navbar-top-links navbar-left m-l-20 hidden-xs">
                     <li>
                         <!-- <form role="search" class="app-search hidden-xs">
