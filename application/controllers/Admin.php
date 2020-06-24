67ea3758141e74f76c125661c85ad0bf6d4e2e14
patheomwiko <patheomwiko45@gmail.com>
2020-06-24 13:46:02 +0200

    modification admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -27,16 +27,5 @@ class Admin extends CI_Controller {
 	}
 	public function deconnexion(){
 		$this->load->view('admin/deconnexion');
-	}
-	function ajouter_vue() {
-		$fichier = dirname(__DIR__). DIRECTORY_SEPARATOR . 'date' . DIRECTORY_SEPARATOR . 'compteur';
-		$compteur = 1;
-		if(file_exists($fichier)){
-			$compteur = (int)file_get_contents($fichier);
-			$compteur++;
-
-		}
-		file_input_contents($fichier,$compteur);
-	}
-	  
+	}	  
 }
