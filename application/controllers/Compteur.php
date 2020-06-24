e6c6a57ef4b244177d0012559a5c03c9cbd8e5a5
patheomwiko <patheomwiko45@gmail.com>
2020-06-24 13:45:15 +0200

    mofication admin

---
+++ application/controllers/Compteur.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Compteur extends CI_Controller {
+ 
+	function ajouter_vue() {
+		$fichier = dirname(__DIR__). DIRECTORY_SEPARATOR . 'date' . DIRECTORY_SEPARATOR . 'compteur';
+		$compteur = 1;
+		if(file_exists($fichier)){
+			$compteur = (int)file_get_contents($fichier);
+			$compteur++;
+
+		}
+		file_input_contents($fichier,$compteur);
+	}
+	  
+}
